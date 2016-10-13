6c5973018138e473251195362571c7eddff2daa1
Jah542 <vince.friedling@gmail.com>
2016-10-13 14:48:58 +0000

    rebuild pages at 417feb4

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
     if (startchange.length){
    $(document).scroll(function() {
       scroll_start = $(this).scrollTop();
-      if(scroll_start < offset.top) {
+      if(scroll_start <= offset.top) {
           $(".site-header").css('opacity', '1');
        } else {
           $('.site-header').css('opacity', '.5');
